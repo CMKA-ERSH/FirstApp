bd0ecc3bb2c44bc2ce4d39d2ceac61c290ebf5bb
CMKA-ERSH <1552069369@qq.com>
2020-04-21 23:43:44 +0800

    SharedPreference+Handler+获取网络数据

--- app/src/main/java/com/swufe/firstapp/RateActivity.java
+++ app/src/main/java/com/swufe/firstapp/RateActivity.java
@@ -11,6 +11,8 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.nfc.Tag;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
@@ -20,15 +22,26 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class RateActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class RateActivity extends AppCompatActivity implements Runnable {
     private static final String TAG = "Rate";
     EditText rmb;
     TextView show;
+    Handler handler;
 
     private float dollarRate = 0.1f;
     private float euroRate = 0.2f;
     private float wonRate = 0.3f;
 
+    @SuppressLint("HandlerLeak")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,10 +56,26 @@ public class RateActivity extends AppCompatActivity {
         dollarRate = sharedPreferences.getFloat("dollar_rate",0.0f);
         euroRate = sharedPreferences.getFloat("euro_rate",0.0f);
         wonRate = sharedPreferences.getFloat("won_rate",0.0f);
+
         Log.i(TAG,"onCreate:sp dollarRate=" + dollarRate);
         Log.i(TAG,"onCreate:sp euroRate=" + euroRate);
         Log.i(TAG,"onCreate:sp wonRate=" + wonRate);
 
+        //开启子线程
+        Thread t = new Thread(this);
+        t.start();
+
+        handler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what == 5) {
+                    String str = (String) msg.obj;
+                    Log.i(TAG, "handlerMessage:getMessage msg = " + str);
+                    show.setText(str);
+                }
+                super.handleMessage(msg);
+            }
+        };
     }
 
     @SuppressLint("DefaultLocale")
@@ -138,4 +167,57 @@ public class RateActivity extends AppCompatActivity {
 
         }
     }
+
+    @Override
+    public void run() {
+        Log.i(TAG,"run:run.......");
+        for(int i=1;i<6;i++){
+            Log.i(TAG,"run:i="+i);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            //获取Msg对象，用于返回主线程
+            Message msg = handler.obtainMessage(5);
+            //msg.what = 5;
+            msg.obj = "Hello from run()";
+            handler.sendMessage(msg);
+
+            //获取网络数据
+            URL url = null;
+            try {
+                url = new URL("http://www.usd-cny.com/icbc.htm");
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            try {
+                assert url != null;
+                HttpURLConnection http = (HttpURLConnection) url.openConnection();
+                InputStream in = http.getInputStream();
+
+                String html = inputStream2String(in);
+                Log.i(TAG, "run:html = " + html);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    private String inputStream2String(InputStream inputStream) throws IOException {
+        final int bufferSize = 1024;
+        final char[] buffer = new char[bufferSize];
+        final StringBuilder out = new StringBuilder();
+        Reader in = new InputStreamReader(inputStream,"gb2312");
+        for( ; ; ){
+            int rsz = in.read(buffer, 0, buffer.length);
+            if(rsz < 0)
+                break;
+            out.append(buffer, 0, rsz);
+        }
+        return out.toString();
+
+    }
+
 }
